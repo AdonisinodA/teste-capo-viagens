c32de4589b9e5139b4a5da57fbdb51f5f52b969b
adonisinoda <adonisinoda07@gmail.com>
2025-07-21 11:44:05 -0300

    chore: adiciona docker-compose

--- src/routes/payments/payments.routes.ts
+++ src/routes/payments/payments.routes.ts
@@ -1,15 +1,17 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import z from "zod";
+import { createPaymentSchema } from "../../schemas/payments/create-payment.schema";
 
 // rotas de pagamentos
 export default function paymentsRoutes(app:FastifyInstance){
     // Criar um pagamento
     app.withTypeProvider<ZodTypeProvider>().post('/', {
         schema:{
+            body:createPaymentSchema,
             response:{
-                200: z.object({
-                          message: z.string(),
+                201: z.object({
+                     message: z.string(),
                  }),
             }
         }
